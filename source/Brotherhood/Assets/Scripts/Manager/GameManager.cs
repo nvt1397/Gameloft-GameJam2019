672b6af84025c81813f4037f5ec4a544c5acffee
Lawless <vanthuan.cab93@gmail.com>
2019-11-10 03:35:48 +0700

    source update

--- source/Brotherhood/Assets/Scripts/Manager/GameManager.cs
+++ source/Brotherhood/Assets/Scripts/Manager/GameManager.cs
@@ -6,15 +6,30 @@ public class GameManager : MonoBehaviour
 {
     GameObject player;
     public GameObject pollutionCloud;
+
+    public GameObject treeManager;
+    public List<GameObject> treePositions;
+
     Vector3 startPos;
     [SerializeField]
     float speed;
     //Fields
+    float totalTreePower;
+
+    [SerializeField]
+    int monsterCount;
+    [SerializeField]
+    float monsterPower;
+
     [SerializeField]
     float currentPollutionVal;  
     int currentHP;
+    [SerializeField]
     int currentCoin;
-
+    //UI
+    public Text currentCoinUI;
+    public Slider HPSlider;
+    public Slider OzoneSlider;
     //Init Game Start
     //player bullet
     public GameObject playerBullet;
@@ -41,51 +56,78 @@ public class GameManager : MonoBehaviour
     //minigun
     public GameObject minigun;
     public ObjectPooling minigunPool;
-    public Slider HPSlider;
-    public Slider OzoneSlider;
+
     // Start is called before the first frame update
     void Awake()
     {
+        //Init tree positions
+        foreach(Transform child in treeManager.transform) {
+            treePositions.Add(child.gameObject);
+         }
+
         player = GameObject.FindGameObjectWithTag("Player");
         startPos = pollutionCloud.transform.position;
+
         //player bullet pool
-        playerBulletPool.InitPool(10,playerBullet);
+        //playerBulletPool.InitPool(10,playerBullet);
 
         //enemy bullet
-        enemyBulletPool.InitPool(10, enemyBullet);
+        //enemyBulletPool.InitPool(10, enemyBullet);
 
         //Enemy1
-        enemy1Pool.InitPool(10, enemy1);
+        //enemy1Pool.InitPool(10, enemy1);
         //Enemy2
         //enemy2Pool.InitPool(10, enemy2);
         //Enemy3
-        enemy3Pool.InitPool(10, enemy3);
+        //enemy3Pool.InitPool(10, enemy3);
 
         //tree
-        treePool.InitPool(10, tree);
+        //treePool.InitPool(10, tree);
 
         //minigun
 
-        minigunPool.InitPool(6, minigun);
+        //minigunPool.InitPool(6, minigun);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(minigunPool.getPoolSize());
+        
         MoveCloud();
-        Mathf.Clamp(currentHP, 0, 100);
-        //HPSlider.value = currentHP;
-        Mathf.Clamp(currentPollutionVal, 0, 100);
-        //OzoneSlider.value = currentOzoneVal;
+        //CalculateTreePower();
+        ChangePollutionVal();
+        UpdateUI();
+        
+    }
+
+    private void UpdateUI()
+    {
+        currentCoinUI.text = currentCoin.ToString();
+        currentHP = Mathf.Clamp(currentHP, 0, 100);
+        HPSlider.value = currentHP;
+        currentPollutionVal = Mathf.Clamp(currentPollutionVal, 0, 100);
+        OzoneSlider.value = 100 - currentPollutionVal;
     }
     private void MoveCloud()
     {
         Vector3 nextPosition = new Vector3(startPos.x,startPos.y - currentPollutionVal*0.15f,startPos.z);
         pollutionCloud.transform.position = Vector3.MoveTowards(pollutionCloud.transform.position, nextPosition, speed * Time.deltaTime);
     }
+
+    private void CalculateTreePower()
+    {
+        foreach(GameObject pos in treePositions)
+        {
+            //GameObject tree = pos.GetComponent<Slot>().tree;          
+            if (tree != null)
+            {
+                TreeGrown treeStat = tree.GetComponent<TreeGrown>();
+                totalTreePower += treeStat.cleanVal - treeStat.pastcleanVal;
+            }
+        }
+    }
     private void ChangePollutionVal() {
-
-
+        currentPollutionVal += ( + (monsterCount * monsterPower) - totalTreePower) * 0.8f * Time.deltaTime;
+        currentPollutionVal = Mathf.Clamp(currentPollutionVal, 0, 100);
     }
 }
---
+++ source/Brotherhood/Assets/Scripts/Tree/TreeGrown.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeGrown : MonoBehaviour
+{
+    int currentHP;
+    [SerializeField]
+    int maxHP = 10;
+
+    int treeState;
+
+    int treeVal;
+    public int cleanVal;
+    public int pastcleanVal;
+    bool canCut;
+
+    Animator anim;
+    [SerializeField]
+    float lifeCycleTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        treeState = 0;
+        anim = GetComponent<Animator>();
+        canCut = false;
+        currentHP = maxHP;
+        cleanVal = 1;
+        pastcleanVal = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        lifeCycleTime += Time.deltaTime;
+        if (lifeCycleTime > 15f) { treeState = 1; treeVal = 4; cleanVal = 3; pastcleanVal = 1;  canCut = true; } //Big Tree
+        else if (lifeCycleTime > 30f) { treeState = 2; treeVal = 3; cleanVal = 2; pastcleanVal = 3; } //Old Tree
+        else if (lifeCycleTime > 40f) { treeState = 3; treeVal = 2; cleanVal = 0; pastcleanVal = 2; } //Dead Tree
+        anim.SetInteger("TreeState", treeState);
+    }
+}
